5bc736ec207352d59516eac6c31d075b053b115a
mariasuha <150585368+mariasuha@users.noreply.github.com>
2024-05-27 20:40:38 +0500

    Add files via upload

---
+++ middlewares/cors.js
@@ -0,0 +1,13 @@
+const allowedCors = ["http://mariasuha-adminka.nomoredomainswork.ru/"];
+
+function cors(req, res, next) {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,HEAD");
+  next();
+}
+
+module.exports = cors;
